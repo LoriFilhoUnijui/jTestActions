9289ca5aa5a46b3099d47841d67c979b75591d47
LoriFilhoUnijui <lori.filho@unijui.edu.br>
2024-09-13 19:56:15 -0300

    Primeiro commit pela IDE

---
+++ src/main/java/edu/br/unijui/jtestactions/JTestActions.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.br.unijui.jtestactions;
+
+/**
+ *
+ * @author lorij
+ */
+public class JTestActions {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
